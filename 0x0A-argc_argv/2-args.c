3fa95dc03fee953060bef00df7328f2008c7ff08
Tihitna-22 <tihitna222sisay@gmail.com>
2021-09-17 03:41:40 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  *main - print argv's arguments.
  *@argc: array length.
  *@argv: array.
  *Return: 0.
  */
-int main(int argc, char* argv[])
-{
-  int i;
-  for(i = 0; i < argc; i++)
-    {
-      printf("%s \n",argv[i]);
-    }
-  return 0;
 
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	while (*(argv))
+	{
+		printf("%s\n", *(argv));
+		argv++;
+	}
+
+	exit(EXIT_SUCCESS);
 }
