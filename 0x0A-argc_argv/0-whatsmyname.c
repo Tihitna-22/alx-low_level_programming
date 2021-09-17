8700bb684638c8d3be85c21fbcc42f098b3447c7
Tihitna-22 <tihitna222sisay@gmail.com>
2021-09-17 03:37:43 -0700

    modify 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  *main - print the name of the program.
  *@argc: array length.
  *@argv: array.
  *Return: 0.
  */
+
+
 int main(int argc, char *argv[])
 {
-  int i;
-  for (i = 0; i < argc; i++)
-    {
-      printf("%s\n", argv[i]);
-    }
-  return 0;
-}
+	(void)argc;
+	printf("%s\n", *argv);
 
+	exit(EXIT_SUCCESS);
+}
