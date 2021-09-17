bea62037ebd0ab18d8d96740115e1c16cc291784
Tihitna-22 <tihitna222sisay@gmail.com>
2021-09-17 00:07:09 -0700

    add  2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - print argv's arguments.
+ *@argc: array length.
+ *@argv: array.
+ *Return: 0.
+ */
+int main(int argc, char* argv[])
+{
+  int i;
+  for(i = 0; i < argc; i++)
+    {
+      printf("%s \n",argv[i]);
+    }
+  return 0;
+
+}
