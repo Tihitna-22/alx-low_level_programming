ba34a3ca20149734cdeb77cfc57a3e22ea3d43dd
Tihitna-22 <tihitna222sisay@gmail.com>
2021-09-25 03:45:21 -0700

    modify 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,22 +12,19 @@
 
 int *array_range(int min, int max)
 {
-	int i, nb_elements, nb;
-	int *p;
-
-	if (min > max)
-		return (NULL);
-
-	nb_elements = max - min + 1;
-        p = malloc(sizeof(int) * nb_elements);
-	if (p == NULL)
-		return (NULL);
-	nb = min;
-	for (i = 0; i < nb_elements; i++)
-	{
-		p[i] = nb;
-		nb++;
-	}
-
-	return (p);
+  int i, nb_elements, nb;
+  int *p;
+if (min > max)
+  return (NULL);
+ nb_elements = max - min + 1;
+ p = malloc(sizeof(int) * nb_elements);
+ if (p == NULL)
+   return (NULL);
+ nb = min;
+ for (i = 0; i < nb_elements; i++)
+   {
+     p[i] = nb;
+     nb++;
+   }
+ return (p);
 }
