5acc71b74e80cf91ea2a473ff0086038ac814bf0
Tihitna-22 <tihitna222sisay@gmail.com>
2021-11-30 02:28:04 -0800

    modify for kernel documentation

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,6 +3,8 @@
 /**
  * dlistint_len - count the number of nodes in the linked list
  * @h: pointer to the beginning of a linked list
+ *
+ * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
