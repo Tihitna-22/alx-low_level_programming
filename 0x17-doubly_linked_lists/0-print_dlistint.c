518ef7a72d161e83a31b8e8e87c316bfbccae7c3
Tihitna-22 <tihitna222sisay@gmail.com>
2021-11-30 02:13:51 -0800

    modify comment for kernel documentation

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,6 +4,7 @@
 /**
  * print_dlistint - print all the elements of a dlistint_t list
  * @h: pointer to the start of the linked list
+ * @return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
