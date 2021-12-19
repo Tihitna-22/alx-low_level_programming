3e2dc10e7d9f44ad899d8981eb7fd6f37b2b6362
Tihitna-22 <tihitna222sisay@gmail.com>
2021-12-19 08:55:28 -0800

    modify for 1-djb2.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,9 +36,8 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-
-
-
+int main(void);
+unsigned long int hash_djb2(const unsigned char *str);
 
 
 #endif /* _HASH_TABLES_H_ */
