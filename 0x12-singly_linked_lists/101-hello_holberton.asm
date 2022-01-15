9c60a0bb8522037ecc7ea18bcda37a0743e04c22
Tihitna-22 <tihitna222sisay@gmail.com>
2022-01-15 09:11:24 -0800

    add 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,12 +6,12 @@ _start:
 	mov edx, 1		; Set the first argument of write (fd 1)
 	mov ecx, msg		; Set the 2nd argument, the text to write
 	mov edx, msglen         ; Set the 3rd argument, lengh to write
-	syscall			; Call write w/ previously defined things
+	int 0x80			; Call write w/ previously defined things
 
 	mov eax, 0x1		; Set the function (syscall) exit (id 60)
 	mov edx, 0		; Set the exit return to be 0
-        syscall			; Execute the syscall exit
+        int 0x80		; Execute the syscall exit
 
 section .data:
-msg: db	"Hello, Holberton", 0xA	; Assign "Hello, world!\n" to the var msg
-msglen: equ $-msg		; Assign len(msg) to msg.len
+        msg: db	"Hello, Holberton", 0xA	; Assign "Hello, world!\n" to the var msg
+        msglen: equ $-msg		; Assign len(msg) to msg.len
