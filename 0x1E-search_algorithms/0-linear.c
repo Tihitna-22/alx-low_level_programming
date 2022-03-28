a657a0e327b8ea3174be837f2e50ad82e73225bf
tihitna-22 <tihitna222sisay@gmail.com>
2022-03-28 02:39:13 -0700

    add 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * size-amount of elements the array contain
+ * value - value to be search
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+  size_t i;
+  int temp;
+
+  for (i = 0; i < size; i++)
+    {
+      temp = array[i];
+      printf("value checked array[%ld] = [%d]\n", i, temp);
+      if (temp == value)
+	return i;
+    }
+      return -1;
+}
